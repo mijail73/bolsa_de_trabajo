0d54941e47051ed0e5b22756cd86acc9d96602f4
Mijail <mijail.iso@ciencias.unam.mx>
2021-03-25 22:22:12 -0600

    routes examples

--- src/App.js
+++ src/App.js
@@ -31,19 +31,6 @@ function App() {
             action={sendForm}
           />
         </Route>
-        <Route path="/registro">REGISTRO</Route>
-        <Route path="/vacantes">VACANTES</Route>
-        <Route path="/acerca">Acerca de nosotros</Route>
-        <Route path="/contacto">
-          <h2 align="center">Contacto</h2>
-          <p align="center">
-            Para cualquier duda que tengas con respecto a la Bolsa de Trabajo y
-            Prácticas Profesionales puedes dirigirte con:
-          </p>
-          <p align="center">Maestro Javier Olguin Huerta, Encargada de BTYPP</p>
-          <p align="center">bolsatradebajofq@unam.mx</p>
-          <p align="center">Teléfono: 55 55 55 55 55</p>
-        </Route>
         <Route path="/" exact>
           <div className="App">
             <header className="App-header">
@@ -62,6 +49,25 @@ function App() {
             </header>
           </div>
         </Route>
+        <Route path="/vacantes">VACANTES</Route>
+        <Route path="/tramites">TRAMITES DE PRACTICAS PROFESIONALES</Route>
+        <Route path="/btfq">EVENTOS DE BTFQ</Route>
+        <Route path="/registro">REGISTRO</Route>
+        <Route path="/info">INFORMACION DEL CORREDOR LABORAL</Route>
+        <Route path="/taller">
+          QUIERO LLEVAR UN TALLER/ACTIVIDAD/WEBINAR A LA FAC
+        </Route>
+        <Route path="/contacto">
+          <h2 align="center">Contacto</h2>
+          <p align="center">
+            Para cualquier duda que tengas con respecto a la Bolsa de Trabajo y
+            Prácticas Profesionales puedes dirigirte con:
+          </p>
+          <p align="center">Maestro Javier Olguin Huerta, Encargada de BTYPP</p>
+          <p align="center">bolsatradebajofq@unam.mx</p>
+          <p align="center">Teléfono: 55 55 55 55 55</p>
+        </Route>
+        <Route path="/acerca">Acerca de nosotros</Route>
       </Switch>
       <Footer />
     </Router>
